71ab9bcbf333b3e9915dcca6bc8fae683b7c9895
Diogo Leite <diogoleite87@gmail.com>
2022-11-08 23:13:24 -0300

    feat: add middleares error treatment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import "express-async-errors";
 import express from 'express'
 import { AppDataSource } from './data-source'
+import { errorMidleware } from './middlewares/error'
 
 AppDataSource.initialize().then(() => {
     const app = express()
 
     app.use(express.json())
 
-    app.get('/', (req, res) => {
+    app.get('/', async (req, res) => {
+
         return res.json('Server Online')
     })
+
+    app.use(errorMidleware)
+
 
     return app.listen(3000)
 })
