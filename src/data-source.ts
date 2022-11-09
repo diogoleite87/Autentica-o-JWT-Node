b99f7c6032a9dabebe28e09357c18abee6291e30
Diogo Leite <diogoleite87@gmail.com>
2022-11-09 13:39:07 -0300

    feat: add entitie user in type orm and migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,5 +8,7 @@ export const AppDataSource = new DataSource({
     port: process.env.DB_PORT as number | undefined,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/**/entities/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 })
